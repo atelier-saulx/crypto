03c5c08ca6a9361877221591b223bdab1b7103a1
Nuno Frade <nfrade.tribal@gmail.com>
2025-04-26 11:45:57 +0200

    Adds Buffer to Uint8Array compatibility test

--- test/index.ts
+++ test/index.ts
@@ -133,3 +133,54 @@ test('should fail with wrong key', async (t) => {
     decrypt(encrypted, priv1)
   })
 })
+
+test.only('Buffer to Uint8Array refactor compatibility', (t) => {
+  const publicKey =
+    '-----BEGIN RSA PUBLIC KEY-----\n' +
+    'MIIBCgKCAQEAu1ze8eitZfmeAY5D3U+NfVjSd33/fpquVxZqP155hZXX3Gt1K4EF\n' +
+    'GkmgRGAqk5VFz9Hq+cE7HNEGM1OAtWbRIqxpgcTi1peZxEg3iSbkMjFLjSoUOrV4\n' +
+    'OR772lky/c4tysrtUznYHau/VnVno+F2LglCxbIZNVs5buWe5TLliLpX5NFHxHdb\n' +
+    'Ekt2nLRRWdlgB2iLUwcCUWug9Eu3v5pqLRpMCKtn6alC/PeHMPbCIWUlflHDlWkx\n' +
+    'C+2+7Ln8BPJtwnQEWoggRGaa+5MamiQy4tU2tGFoDf+lRSmjmYyz9/YDOVTGvXqi\n' +
+    '3bu3S/qGIoeXtFAkBhinsD5+D6EY/RrdRQIDAQAB\n' +
+    '-----END RSA PUBLIC KEY-----\n'
+  const privateKey =
+    '-----BEGIN RSA PRIVATE KEY-----\n' +
+    'MIIEpAIBAAKCAQEAu1ze8eitZfmeAY5D3U+NfVjSd33/fpquVxZqP155hZXX3Gt1\n' +
+    'K4EFGkmgRGAqk5VFz9Hq+cE7HNEGM1OAtWbRIqxpgcTi1peZxEg3iSbkMjFLjSoU\n' +
+    'OrV4OR772lky/c4tysrtUznYHau/VnVno+F2LglCxbIZNVs5buWe5TLliLpX5NFH\n' +
+    'xHdbEkt2nLRRWdlgB2iLUwcCUWug9Eu3v5pqLRpMCKtn6alC/PeHMPbCIWUlflHD\n' +
+    'lWkxC+2+7Ln8BPJtwnQEWoggRGaa+5MamiQy4tU2tGFoDf+lRSmjmYyz9/YDOVTG\n' +
+    'vXqi3bu3S/qGIoeXtFAkBhinsD5+D6EY/RrdRQIDAQABAoIBAFHubjWl1HGj4tL2\n' +
+    'Vbno6Ev4c+y55eiElplRnXuBgi2G1YK3YOD5xfP1X0aXMPchjwouVw0JUSKsSwRV\n' +
+    'zxJEWE7Ly0VqhfFmEOEy3Uo1/hLu2IVt8bOsmFqOMH8Og4xWRVMJQxeiU13CNWUG\n' +
+    '6R/SX34JIbBWzcw1zJswgTfj9li/uFXd4i7NRmCH2g7gd9ecmId8mbl3moggUd9b\n' +
+    'rgYmLBKCFqS7loEmSKNUDkzDnsGBkre8SKzuU9gqjvuDAlkLnrWyi92RqDVv2vof\n' +
+    'PlGrD+DNkKYzRVpRASd3mPRxcYXfTuNXKcmNfSmMjE7y0V6+BK8vYN0hldZOuVDM\n' +
+    '+wLx6v8CgYEA2xstRYlOfqPpUfuSOW7l1Y7hfemhOqnxJ1DeBZqfMGNOWsLOljMx\n' +
+    '+5wYQU2ys0jr9o4zk9LGhshMY1Z7XcZha4YDZPFm7GYJwowEsGqpRdVkjwu+eisS\n' +
+    'XLaRkTNbhi6zleZuXMyqmW/cvfbhIFuqB5kGn/uD/ilV3rkEIZJ/LksCgYEA2ulb\n' +
+    '5yQKvhssKVkt7o7tKWE1FnpV7b75Ey/QyKSn4YWaNlxQI6ReuXnMHrb0nTlYre/d\n' +
+    'AmStDAdtwHSHJQWLWh0p+1LNUR9jZNms/NzKX6prtOLEZ4OsafKu/Vb9SwnHKUyl\n' +
+    'ivjfO+mVZ6XVID99gB/AxJbpHzVWpprchBO7qK8CgYEAqQSG4Lwxo/lXU9mni8xj\n' +
+    'WWE1ywZ9TB6qG3UDP/lt0UrZt6PM7wqhBQH9p+qC7lBTTceWO/L9GB3M51hgJA+T\n' +
+    'OPmRBr49ciCoaL3QJDKzT150ivA5SE6PhJuFISgn8xN1jy3Jdqae92vWMWgPdAGh\n' +
+    '4OiHtsOzp01Fo2C/shIrL48CgYEAvMCRmY8eJCTRwyU90YAzRYane6YoCUKtCLol\n' +
+    'z5sJlg7YlR6ris/jTRGTyrf92sLjj7ZOXg3ol4KgQ102WdmZ+i1DUYxntBbYYNC/\n' +
+    's5e8dxg+nk0ZBBgoxYGXGKG07MqViyqnYliMQuB5DlGSnqef0qWOVPO6NkqLWEQW\n' +
+    '91r8Z8kCgYAR9AIwGHxGOelsP1QTyrh1SF37IXA1YsvOGp0c7+JwdYgVgWesLf05\n' +
+    'LxlU60/DXvjAdpwrk0mn5MVYnwXEUysaPr4GOw7kBcQg1g6gKKpW2N0Gx+hrJCak\n' +
+    'msSmG79Dz2AAEdHTrdjGBN0p0Bh1JkPU/5W4VHAo1kzYbyfrMrCuoA==\n' +
+    '-----END RSA PRIVATE KEY-----\n'
+  const signed =
+    'ggfMH51zJiLHKxVxh1WeOlFXcW1O33xPRdEJZGzR3teiV6KtQ1MAN03hfqdcii99NKZkqGx0pdq6PWBCS+c3l6wS+MYYgqAtEhWfULcB6eTTCgqXIdOYVZhgT3n5/EeMIK6q1N6OVYdXzP5drQijBruGUFQpP53O9mkfLa4EzNC68b9xk4LRMi0u5ajb6k2sQTYjLjT3jcZ5kt6T0Rdftvzl1ATUKGZE8EZS3tARBB3CXYz4HmJHhYP7n3BV+j1pMsbaG/FAHVkqrVfn6TLiFIw58msRrMsDNVDI+7FA1d914Gxp66IFK+xSqGrxThPhJ2Zv8dzGvztPT5spwAsQmXsiaWQiOjIyMzM0LCJleHAiOjE3NDU2NjAxMDI1Nzh9ag=='
+  const encrypted =
+    'NKFErwJ+4Jz7Lk4XMzSrmEBU6RSs5bxrun8oBdXoBfVQsCHTKEExnvKFAz0W3IN4ugFDwlQP8F7xYQWsi6UdgaKmeOf2SqWnGx26krsFJKR4fYSnSkTJHIzesU4PCyeHscbpqhHOUiFsN5ulUog+GzRt6f59gnk7+B28yEfeqJV4YBaw2Z0siXhNr+5f15h7Kj218qRsl01DrCJNI/xa5R1ZuQcKYlE9AiEHZcaB3jgEdDr8AOWvD0zbrkqyrD/n11MPJi95lq3hPvwbzXpOtlE7STmD0IPOoKL8SP1iZ1VcHmHQnmmYoAdc+5Kx2/ubqEgBu2tGeNYinbzvVkalFuo88lPup8fgRfpj4tLjBbuqvHQ+ba6GgoLVwZ0LwdBv'
+
+  const id = 22334
+  const exp = 1745660102578
+  let body = { id, exp }
+
+  t.deepEqual(verify(signed, publicKey), body)
+  t.deepEqual(decrypt(encrypted, privateKey), JSON.stringify(body))
+})
